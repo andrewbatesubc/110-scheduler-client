33f7a7a863b393bd646b23da8aa12305a1b8d65d
Andrew Bates <andrewbatesubc@gmail.com>
2017-12-15 09:03:20 -0800

    UI and button updates based on feedback

---
+++ Assets/Scripts/UI/HelpPanelHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpPanelHelper : MonoBehaviour {
+
+    public GameObject panel;
+
+    void Awake() {
+        if (PlayerPrefs.GetInt("hasBeenHelped") != 1) {
+            panel.SetActive(true);
+        }
+    }
+
+    public void HandleHasBeenHelped() {
+        PlayerPrefs.SetInt("hasBeenHelped", 1);
+    }
+}
